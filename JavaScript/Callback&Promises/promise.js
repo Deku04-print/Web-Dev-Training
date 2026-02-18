6791f3f7bdfebb8526689b3ac37874f7d2bececa
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-02-19 04:10:37 +0530

    Promises done

--- JavaScript/Callback&Promises/promise.js
+++ JavaScript/Callback&Promises/promise.js
@@ -1,14 +1,51 @@
-let prom=new Promise((resolve,reject)=>{
-    let ans = 12+12;
-    if(ans==25){
-        resolve("Resolved...!!")
-    }else{
-        reject("Rejectedd......$$")
-    }
-})
+//Promises in js
 
-prom.then((a)=>{
-    console.log(a)
-}).catch((err)=>{
-    console.log(err)
-})
+function walkDog(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+
+        let walkDo=true;
+
+        if(walkDo){
+            resolve("Happy happyy happyyy");
+        }else{
+            reject("Dog Rejectedd you");
+        }        
+        },1500);
+    });
+}
+
+function kitchen(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+
+        let doneKi=true;
+
+        if(doneKi){
+            resolve("Kitchen Done....!!Happy happyy happyyy");
+        }else{
+            reject("kitchen not done");
+        }        
+        },3500);
+    });
+}
+
+function takeTrash(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+
+        let takeTra=true;
+
+        if(takeTra){
+            resolve("Trash Done....!!Happy happyy happyyy");
+        }else{
+            reject("Go complete the incomplete....");
+        }        
+        },4500);
+    });
+}
+
+walkDog().then(value=>{console.log(value); return kitchen()})
+        .then(value=>{console.log(value); return takeTrash()})
+        .then(value=>{console.log(value); console.log("Finisheddd....")})
+        .catch(error=>console.log(error));        
