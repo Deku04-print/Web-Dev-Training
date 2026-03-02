d694125e1562e2ed3685dbe12d7d82f94c6a0bfa
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-03-03 04:06:23 +0530

    Implimented the fetch() using Async/await

--- JavaScript/Callback&Promises/AsyncAwait/MiniProject/script.js
+++ JavaScript/Callback&Promises/AsyncAwait/MiniProject/script.js
@@ -1,14 +1,58 @@
 const btnEl = document.querySelector("#btn")
+const print = document.querySelector(".pri")
+const newUser={
+    name:'john',
+    job:'carpenter'
+}
 
-const clickHandler = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res =>
-        res.json()
-    ).then(data=>{
+const clickHandler =async () => {
+
+    try{
+        const res = await fetch('https://jsonplaceholder.typicode.com/users',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(newUser)
+        })
+        const data = await res.json();
+
+        if(!res.ok){
+            console.log(data.description);
+            return
+        }
         console.log(data)
-    }).catch(err =>{
-        console.log("Error : ",err)
-    })
+
+    }catch(error){
+        console.log(error)
+    }
+
+
+    // try{
+
+    //     const res=await fetch('https://jsonplaceholder.typicode.com/users')
+    //     const data =await res.json();
+    //     console.log(data[0].name)
+    //         print.innerText=data[0].name
+
+
+    // }catch(error){
+    //     console.log("Error : ",err)
+    // }
+        
+
+
+
+
+
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    // .then(res =>
+    //     res.json()
+    // ).then(data=>{
+    //     console.log(data)
+    // }).catch(err =>{
+    //     console.log("Error : ",err)
+    // })
 }
 
 
