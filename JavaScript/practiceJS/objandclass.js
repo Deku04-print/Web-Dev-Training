9687f6338814cf881a13293004ead4a27dd7b484
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-03-10 03:02:22 +0530

    Done for the day

--- JavaScript/practiceJS/objandclass.js
+++ JavaScript/practiceJS/objandclass.js
@@ -11,3 +11,45 @@
 // random.__proto__=harsh
 
 // console.log(random.eats)
+
+
+class Animal{
+    constructor(name) {
+        this.name = name
+        console.log("Object is created....!")
+        
+    }
+
+    eats(){
+        console.log("I am eating")
+
+    }
+
+    jumps(){
+        console.log("Jumping jumping")
+    }
+    
+}
+
+class Lion extends Animal{
+    constructor(name){
+        super(name)
+        console.log("Animal constructor got called")
+
+    }
+
+    eats(){
+        console.log("Roarrrr")
+    }
+
+}
+
+let a=new Animal("bunny");
+let b=new Animal("cat");
+
+let c=new Lion("Lion");
+
+console.log(a.name)
+console.log(b.name)
+console.log(c)
+console.log(c.eats)
