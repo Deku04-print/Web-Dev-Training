d494c1a88610400b06fdea1f95244ee6bd542377
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-03-10 01:58:06 +0530

    Commit of the day

---
+++ JavaScript/practiceJS/objandclass.js
@@ -0,0 +1,13 @@
+// let harsh ={
+//     name:"Harsh D",
+//     id:1,
+//     eats:true
+// }
+// let random ={
+//     name:"random D",
+//     id:2
+// }
+
+// random.__proto__=harsh
+
+// console.log(random.eats)
