9c260444b1c2ac32b322dbc038246311c968ae58
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-03-24 05:12:04 +0530

    Solving some js problems 2

--- JavaScript/advJs/script.js
+++ JavaScript/advJs/script.js
@@ -12,9 +12,36 @@ async function sleep() {
     })
 }
 
+function sum(a,b,c,d){
+    return a+b+c+d
+
+}
+
 (async function main(){ 
-let a =await sleep()
-console.log(a)
-let b= await sleep()
-console.log(b)
+// let a =await sleep()
+// console.log(a)
+// let b= await sleep()
+// console.log(b)
+
+
+let [x,y,z,...rest]=[1,5,6,7,8,9,10]
+console.log(x,y,z)
+console.log(rest)
+
+// let obj={
+//     a:1,
+//     b:2,
+//     c:3
+
+// }
+
+// let {a,b} = obj
+// console.log(a,b)
+
+let arr=[1,4,6,4]
+console.log(sum(arr[0],arr[1],arr[2],arr[3]))
+console.log(sum(...arr))
+
 })()
+
+
---
+++ JavaScript/advJs/script2.js
@@ -0,0 +1,26 @@
+
+// sum of even numbers in array
+
+/* let arr=[3,2,5,6,4,6,7,2]
+let sum=0;
+
+for (const num of arr){
+    
+        if(num%2==0){
+        sum+=num;
+    
+    }
+    
+}
+console.log(sum) */
+
+//Reversed string 
+
+/*let string1="Harsh"
+
+let newstr=''
+for(let char=string1.length-1;char>=0;char--){
+    newstr+=string1[char]
+
+}
+console.log(newstr) */
